3b79f6dfea421ee5911575aba630b8e05f385065
Milanesa-chan <lucagutierrez@hotmail.com>
2025-02-28 00:31:25 -0300

    Changed positioning logic to try to fix topbar.

--- js/main.js
+++ js/main.js
@@ -1,24 +1,37 @@
-document.addEventListener("DOMContentLoaded", () => {
-    gsap.registerPlugin(ScrollTrigger);
+var $mainIcon;
+var $header;
 
-    crearAnimacion();
-    window.addEventListener("resize", crearAnimacion);
+document.addEventListener("DOMContentLoaded", () => {
+    $mainIcon = document.getElementById("main-icon");
+    $header = document.getElementById("header");
+
+    gsap.registerPlugin(ScrollTrigger);
 
     setVH();
     window.addEventListener('resize', setVH);
+
+    crearAnimacion();
+    window.addEventListener("resize", crearAnimacion);
 });
 
 function crearAnimacion() {
-    let endPosition = (window.innerHeight * 0.4) - (window.innerWidth > 600 ? 100 : 40);
+    ScrollTrigger.refresh();
+
+    let endPosition = (window.innerHeight * 0.4) - (window.innerWidth > 600 ? 100 : 80);
     let scale = window.innerWidth > 600 ? 0.3 : 0.5;
-    let top = (window.innerHeight * 0.4) - (window.innerWidth > 600 ? 50 : 40);
+    let top = (window.innerHeight * 0.4) - (window.innerWidth > 600 ? 50 : 40) - ($mainIcon.height / 2);
+
+    console.log($header.offsetHeight);
+    console.log($mainIcon.offsetHeight);
+    let startingTop = $header.offsetHeight - ($mainIcon.offsetHeight / 2);
     // Eliminar triggers antiguos para evitar duplicaciones
     ScrollTrigger.getAll().forEach(t => t.kill());
+
+    gsap.set(".main-icon", { top: startingTop });
 
     gsap.to(".main-icon", {
         scale: scale,
         top: top,
-        transform: `translateY(-50%)`,
         scrollTrigger: {
             trigger: "body",
             start: "top",
@@ -29,6 +42,9 @@ function crearAnimacion() {
 
     // Refrescar ScrollTrigger después de actualizar
     ScrollTrigger.refresh();
+
+    console.log("RESIZE");
+    
 }
 
 // Ajustar --vh en función del tamaño real del viewport
